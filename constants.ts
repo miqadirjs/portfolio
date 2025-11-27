9b40354fec0a737845a53d18ae14dd0c68ed1661
Imtiaz <imme.pk@gmail.com>
2025-11-28 01:55:18 +0500

    ver2

--- constants.ts
+++ constants.ts
@@ -5,7 +5,7 @@ export const PROJECTS: Project[] = [
     id: 1,
     title: "Lumina Interface",
     category: "UI/UX Design",
-    image: "/images/3.jpg",
+    image: "https://res.cloudinary.com/dfatwlbay/image/upload/v1764276265/xacf0g5xszzv8bz73uqu.jpg",
     year: "2024",
     description: "A comprehensive design system for a fintech startup, focusing on clarity and dark-mode aesthetics."
   },
@@ -13,7 +13,7 @@ export const PROJECTS: Project[] = [
     id: 2,
     title: "Apex Architecture",
     category: "Web Development",
-    image: "/images/2.jpg",
+    image: "https://res.cloudinary.com/dfatwlbay/image/upload/v1764276266/xvqb3swdpfdgaz5jb3cp.jpg",
     year: "2023",
     description: "Minimalist portfolio website for an architectural firm featuring WebGL 3D visualizations."
   },
@@ -21,7 +21,7 @@ export const PROJECTS: Project[] = [
     id: 3,
     title: "Neon Commerce",
     category: "E-Commerce",
-    image: "/images/4.jpg",
+    image: "https://res.cloudinary.com/dfatwlbay/image/upload/v1764276266/uxcmsnfutoradb4zyxhx.jpg",
     year: "2023",
     description: "High-performance headless Shopify store with custom animations and fluid transitions."
   },
@@ -29,7 +29,15 @@ export const PROJECTS: Project[] = [
     id: 4,
     title: "Echo AI",
     category: "Product Design",
-    image: "/images/ai-post.jpg",
+    image: "https://res.cloudinary.com/dfatwlbay/image/upload/v1764276265/xclybrswb705qqi3zlhz.jpg",
+    year: "2024",
+    description: "Dashboard interface for an AI-powered analytics tool."
+  },
+   {
+    id: 4,
+    title: "Echo AI",
+    category: "Product Design",
+    image: "https://res.cloudinary.com/dfatwlbay/image/upload/v1764276265/to8fau65jos1g4ryescs.jpg",
     year: "2024",
     description: "Dashboard interface for an AI-powered analytics tool."
   }
